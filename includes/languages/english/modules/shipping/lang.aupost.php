f85c8aeb2448fa44788a7a39145931a8e31de09e
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2022-08-09 14:38:16 +1000

    Add files via upload
    ZC 158 language files for aupost and aupostoverseas

---
+++ includes/languages/english/modules/shipping/lang.aupost.php
@@ -0,0 +1,14 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_AUPOST_TEXT_TITLE' => 'Australia Post',
+    'MODULE_SHIPPING_AUPOST_TEXT_DESCRIPTION' => 'Australia Post Shipping Module',
+    'MODULE_SHIPPING_AUPOST_TEXT_ERROR' => '<font color="#FF0000">Estimate only:</font> We were unable to obtain a valid quote from the Australia Post Server.<br />You may still checkout using this method or contact us for accurate postage costs.',
+];
+// BMH additional defines to avoid PHP errors
+// define('MODULE_SHIPPING_AUPOST_STATUS','');
+// define('MODULE_SHIPPING_AUPOST_SORT_ORDER','');
+// define('MODULE_SHIPPING_AUPOST_ICONS','');
+// define('MODULE_SHIPPING_AUPOST_CORE_WEIGHT', '');
+// define('MODULE_SHIPPING_AUPOST_HIDE_PARCEL', '');
+
+return $define;
