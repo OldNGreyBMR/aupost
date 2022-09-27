b85a2b575e044e35955d68859cb013e3cecfb629
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2022-09-28 08:01:33 +1000

    Add files via upload
    v2.4.2

--- includes/languages/english/modules/shipping/aupost.php
+++ includes/languages/english/modules/shipping/aupost.php
@@ -1,13 +1,14 @@
 <?php
-/*
-  $Id: aupost.php,v 2.2 2016/11/02
-
-  Copyright (c) 2007-2009 Rod Gasson / VCSWEB
-
+/*   Original Copyright (c) 2007-2009 Rod Gasson / VCSWEB
   Released under the GNU General Public License
 */
-// BMH 2022-01-16
+/*   $Id: aupost.php,v 2.4.2 2022-09-28
+ updated to v2.4.2 BMH 2022-09-28
+ */
 define('MODULE_SHIPPING_AUPOST_TEXT_TITLE'	, 'Australia Post');
 define('MODULE_SHIPPING_AUPOST_TEXT_DESCRIPTION' , 'Australia Post Shipping Module') ;
 define('MODULE_SHIPPING_AUPOST_TEXT_ERROR', '<font color="#FF0000">Estimate only:</font> We were unable to obtain a valid quote from the Australia Post Server.<br />You may still checkout using this method or contact us for accurate postage costs.');
 
+//// LETTERS – Australia Post (national & international) //
+define('LETTER_SMALL_description', 'Small Letter-Regular (NO TRACKING)') ;
+define('LETTER_LARGE_description', 'Large Letter (NO TRACKING) ;
