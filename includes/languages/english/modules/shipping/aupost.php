e88bebec8cebb4780ea262cf3f2337636b4a5a9c
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2023-03-22 16:11:47 +1000

    Add files via upload

--- includes/languages/english/modules/shipping/aupost.php
+++ includes/languages/english/modules/shipping/aupost.php
@@ -1,14 +1,19 @@
 <?php
-/*   Original Copyright (c) 2007-2009 Rod Gasson / VCSWEB
+/*
+  $Id: aupost.php,v 2.2 2016/11/02
+
+  Copyright (c) 2007-2009 Rod Gasson / VCSWEB
+
   Released under the GNU General Public License
 */
-/*   $Id: aupost.php,v 2.4.2 2022-09-28
- updated to v2.4.2 BMH 2022-09-28
- */
+// BMH 2022-01-16
 define('MODULE_SHIPPING_AUPOST_TEXT_TITLE'	, 'Australia Post');
 define('MODULE_SHIPPING_AUPOST_TEXT_DESCRIPTION' , 'Australia Post Shipping Module') ;
 define('MODULE_SHIPPING_AUPOST_TEXT_ERROR', '<font color="#FF0000">Estimate only:</font> We were unable to obtain a valid quote from the Australia Post Server.<br />You may still checkout using this method or contact us for accurate postage costs.');
+define('MSGLETTERTRACKING', ' <b>(No tracking)</b>');
 
-//// LETTERS – Australia Post (national & international) //
-define('LETTER_SMALL_description', 'Small Letter-Regular (NO TRACKING)' ) ;
-define('LETTER_LARGE_description', 'Large Letter (NO TRACKING)' ) ;
+//define('MODULE_SHIPPING_AUPOST_STATUS','');
+//define('MODULE_SHIPPING_AUPOST_SORT_ORDER','');
+//define('MODULE_SHIPPING_AUPOST_ICONS','');
+//define('MODULE_SHIPPING_AUPOST_TAX_CLASS','');
+//define('MODULE_SHIPPING_AUPOST_TYPES1','');
--- includes/languages/english/modules/shipping/aupostoverseas.php
+++ includes/languages/english/modules/shipping/aupostoverseas.php
@@ -5,8 +5,16 @@
   Copyright (c) 2007-2009 Rod Gasson / VCSWEB
 
   Released under the GNU General Public License
-*/ /* BMH 2022-01-20 */
+*/ /* BMH 2023-031-12 */
 
 define('MODULE_SHIPPING_OVERSEASAUPOST_TEXT_TITLE'	, 'Australia Post International');
 define('MODULE_SHIPPING_OVERSEASAUPOST_TEXT_DESCRIPTION' , 'Australia Post International Rates Shipping Module') ;
 define('MODULE_SHIPPING_OVERSEASAUPOST_TEXT_ERROR', '<font color="#FF0000">Estimate only:</font> We were unable to obtain a valid quote from the Australia Post Server.<br />You may still checkout using this method or contact us for accurate postage costs.');
+define('MSGNOTRACKING', '<b> (No tracking) </b>');         // label append // emphasis to minimise complaints
+
+//define('MODULE_SHIPPING_OVERSEASAUPOST_STATUS','');
+//define('MODULE_SHIPPING_OVERSEASAUPOST_SORT_ORDER','');
+//define('MODULE_SHIPPING_OVERSEASAUPOST_ICONS','');
+//define('MODULE_SHIPPING_OVERSEASAUPOST_TAX_CLASS','');
+//define('MODULE_SHIPPING_OVERSEASAUPOST_TAX_CLASS','');
+
