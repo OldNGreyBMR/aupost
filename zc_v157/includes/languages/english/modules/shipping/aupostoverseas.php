31b7a7a386a833fb774822148b09ee8b301a9339
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2025-07-02 16:36:34 +1000

    Add files via upload

---
+++ zc_v157/includes/languages/english/modules/shipping/aupostoverseas.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  $Id: aupost.php,v 2.5.8 
+  Released under the GNU General Public License
+*/ /* BMH 2025-07-01 */
+
+define('MODULE_SHIPPING_OVERSEASAUPOST_TEXT_TITLE'	, 'Australia Post International');
+define('MODULE_SHIPPING_OVERSEASAUPOST_TEXT_DESCRIPTION' , 'Australia Post International Rates Shipping Module') ;
+define('MODULE_SHIPPING_OVERSEASAUPOST_TEXT_ERROR', '<font color="#FF0000">Estimate only:</font> We were unable to obtain a valid quote from the Australia Post Server.<br />You may still checkout using this method or contact us for accurate postage costs.');
+define('MSGNOTRACKING_INT', '<b> (No tracking) </b>');         // label append // emphasis to minimise complaints
+
