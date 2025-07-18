bdc98419f3b0bc817c8295fd937db9e3100eaaf0
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2025-07-18 13:02:15 +1000

    Update manifest.php

--- zc_plugins/AustraliaPost/V2.5.8b/manifest.php
+++ zc_plugins/AustraliaPost/V2.5.8b/manifest.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'pluginVersion' => 'v2.5.8a',
+    'pluginVersion' => 'v2.5.8b',
     'pluginName' => 'Australia Post',
     'pluginDescription' => 'Obtain postage quoted from Australia Post servers.',
     'pluginAuthor' => 'OldNGrey (BMH)',
