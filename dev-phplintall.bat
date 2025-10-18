e058022d99c591c7b85f6ceedb9aeb6d8eb11944
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2025-10-18 16:14:38 +1000

    Delete dev-phplintall.bat

--- dev-phplintall.bat
+++
@@ -1,7 +0,0 @@
-echo checking php using php -l
-echo php files error check report %date% %time%  >dev-output.txt
-echo
-php -v  >>dev-output.txt
-echo
-for /r %%i in (*.php) do ( echo.  & php -l "%%i"  )  >>dev-output.txt
-start "" "dev-output.txt" win
